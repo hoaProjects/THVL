411a397936e0ec0b3a2e295075b5085be5f48499
binhdarkcu <binhdarkcu@gmail.com>
2017-04-11 18:11:08 +0700

    responsive home page

--- app/components/Common/SliderList.js
+++ app/components/Common/SliderList.js
@@ -48,7 +48,25 @@ export default class categoryList extends React.Component {
             centerMode: true,
             centerPadding: '3px',
             prevArrow: <PrevButton />,
-            nextArrow: <NextButton />
+            nextArrow: <NextButton />,
+            responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                  }
+                },
+                {
+                  breakpoint: 641,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                }
+              ]
         };
         return (
             <div className="sliderBlock">
