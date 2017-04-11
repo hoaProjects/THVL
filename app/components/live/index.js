3d91f28a399b84340b069bc15fb73522abc9d9c0
hoapham <hoapham@bliss-interactive.net>
2017-04-11 22:56:29 +0700

    update code

--- app/components/live/index.js
+++ app/components/live/index.js
@@ -12,17 +12,16 @@ export default class LiveHome extends React.Component {
         this.state = {
             data:
             [
-                { 'src': '../app/assets/img/live/THVL1HD.png'},
-                { 'src': '../app/assets/img/live/Vinh-Long-2-HD.png'},
-                { 'src': '../app/assets/img/live/Vinh-Long-Cai-Luong.png'},
-                { 'src': '../app/assets/img/live/Vinh-Long-Ca-Nhac.png'},
-                { 'src': '../app/assets/img/live/Vinh-Long-Dien-Anh.png'},
-                { 'src': '../app/assets/img/live/HTV-1.png'},
-                { 'src': '../app/assets/img/live/HTV-2.png'},
-                { 'src': '../app/assets/img/live/HTV-3.png'},
-                { 'src': '../app/assets/img/live/HTV-4.png'},
-                { 'src': '../app/assets/img/live/HTV-7.png'},
-                { 'src': '../app/assets/img/live/HTV-2.png'},
+                { 'src': '../app/assets/img/live/live-tv01.jpg'},
+                { 'src': '../app/assets/img/live/live-tv02.jpg'},
+                { 'src': '../app/assets/img/live/live-tv03.jpg'},
+                { 'src': '../app/assets/img/live/live-tv04.jpg'},
+                { 'src': '../app/assets/img/live/allLive.jpg'},
+                { 'src': '../app/assets/img/live/live-tv01.jpg'},
+                { 'src': '../app/assets/img/live/live-tv02.jpg'},
+                { 'src': '../app/assets/img/live/live-tv03.jpg'},
+                { 'src': '../app/assets/img/live/live-tv04.jpg'},
+                { 'src': '../app/assets/img/live/allLive.jpg'}
             ]
         }
     }
