789e75127352e6a306f75f415984d3bf28e71e53
binhdarkcu <binhdarkcu@gmail.com>
2017-04-11 20:42:12 +0700

    responsive homepage

--- app/components/Common/Navigation.js
+++ app/components/Common/Navigation.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Navigation = () =>
-  <nav className="main-nav">
-      <Link to="/" activeClassName="active">Trang chủ</Link>
-      <Link to="/truyen-hinh" activeClassName="active">Truyền hình</Link>
-      <Link to="/tv-show" activeClassName="active">TV SHOW</Link>
-      <Link to="/cai-luong" activeClassName="active">Cải lương</Link>
-      <Link to="/phim-viet-nam" activeClassName="active">Phim việt nam</Link>
-      <Link to="/phim-nuoc-ngoai" activeClassName="active">Phim nước ngoài</Link>
-      <Link to="/thieu-nhi" activeClassName="active">thiếu nhi</Link>
-      <Link to="/ca-nhac" activeClassName="active">ca nhạc</Link>
-  </nav>;
+export class Navigation extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            showHideMobileButton: "hidden"
+        }
+    }
+    getInitialState=() => {
+        return {"showHideMobileButton":"hidden"};
+    }
+    handleClick=(nav)=> {
+        var css = (this.state.showHideMobileButton === "hidden") ? "show" : "hidden";
+        this.setState({"showHideMobileButton":css});
+    }
+    render(){
+        return (
+            <nav className="main-nav">
+                <a className="mobileButton" href="#" onClick={this.handleClick.bind(this)}></a>
+                <div className={this.state.showHideMobileButton + ' mainNavigation'}>
+                    <Link to="/" activeClassName="active">Trang chủ</Link>
+                    <Link to="/truyen-hinh" activeClassName="active">Truyền hình</Link>
+                    <Link to="/tv-show" activeClassName="active">TV SHOW</Link>
+                    <Link to="/cai-luong" activeClassName="active">Cải lương</Link>
+                    <Link to="/phim-viet-nam" activeClassName="active">Phim việt nam</Link>
+                    <Link to="/phim-nuoc-ngoai" activeClassName="active">Phim nước ngoài</Link>
+                    <Link to="/thieu-nhi" activeClassName="active">thiếu nhi</Link>
+                    <Link to="/ca-nhac" activeClassName="active">ca nhạc</Link>
+                </div>
+            </nav>
+        );
+    }
+}
 export default Navigation;
--- app/components/Common/SliderList.js
+++ app/components/Common/SliderList.js
@@ -48,7 +48,23 @@ export default class categoryList extends React.Component {
             centerMode: true,
             centerPadding: '3px',
             prevArrow: <PrevButton />,
-            nextArrow: <NextButton />
+            nextArrow: <NextButton />,
+            responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                  }
+                },
+                {
+                  breakpoint: 641,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                }
+              ]
         };
         return (
             <div className="sliderBlock">
