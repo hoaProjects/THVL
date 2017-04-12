80d6e847427ffe333ad6db6915fccee3bdc1fc01
hoapt.info <hoapt.info@gmail.com>
2017-04-12 14:42:14 +0700

    tablet responsive

--- app/components/Common/sliderListLive.js
+++ app/components/Common/sliderListLive.js
@@ -12,7 +12,23 @@ export default class SliderListLive extends React.Component {
           infinite: true,
           speed: 500,
           slidesToShow: 5,
-          slidesToScroll: 5
+          slidesToScroll: 5,
+          responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3
+                }
+              },
+              {
+                breakpoint: 641,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              }
+            ]
         };
         return (
             <section className="listSlider">
